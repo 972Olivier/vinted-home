e24bd0e4742e4917b826b333c0f4d8fc167fb167
972Olivier <oliviercen@gmail.com>
2022-07-30 19:35:17 +0200

    catch the value on submit ok

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,22 +5,81 @@ import { Link } from "react-router-dom";
 import "./Signup.css";
 
 const Signup = () => {
+  /*--state par input--*/
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newsletter, setnewsletter] = useState(false);
+  /*---fonction par input pour récuperer leur value---*/
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  /*--------récupérer les données lors du submit---------*/
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email, password, newsletter);
+  };
+  let objectUser = {
+    email: email,
+    username: name,
+    password: password,
+    newsletter: newsletter,
+  };
+  console.log(objectUser);
   return (
     <article>
       <h1>S'inscrire</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="lineUnderInput">
-          <input type="text" placeholder="Nom d'utilisateur" />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Nom d'utilisateur"
+            onChange={handleNameChange}
+          />
         </div>
         <div className="lineUnderInput">
-          <input type="email" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={handleEmailChange}
+          />
         </div>
         <div className="lineUnderInput">
-          <input type="password" placeholder="Mot de passe" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Mot de passe"
+            onChange={handlePasswordChange}
+          />
         </div>
         <div className="newsletter">
           <div>
-            <input type="checkbox" name="" id="" />
+            <input
+              onClick={() => {
+                newsletter === false
+                  ? setnewsletter(true)
+                  : setnewsletter(false);
+              }}
+              type="checkbox"
+              name="newsletter"
+              value={newsletter}
+              //   onChange={handleNewsletterChange}
+            />
           </div>
           <p>S'inscrire à notre newsletter</p>
         </div>
